2a7407259b354a47b495e2c72764c4888bb60bf6
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 15:00:27 -0500

    Add files via upload

---
+++ reactNativeTest/test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent: "center",
+        alignIteams: "center"
+      }}>
+      <Text>Hello, World!</Text>
+    </View>
+  )
+}
+export default HelloWorldApp;
